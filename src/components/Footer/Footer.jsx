2ced7bbc186bc7a8636f69ac7102876e4e525a55
Guilherme Santos <guilhermejjw@gmail.com>
2026-09-12 22:28:45 -0300

    feat: adição de informação sobre autor

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,9 @@ function Footer() {
     // Tag semântica para indicar o rodapé da aplicação
     <footer>
       <p>Missão concluída: projeto desenvolvido para aprender React.js.</p>
+      <p>
+        Autor: Guilherme Santos
+      </p>
     </footer>
   );
 }
